f301d2d98a075601525adf200365c3a34f62af5e
세빈아빠집_인텔리제이 <sebinpapa@outlook.kr>
2023-04-02 10:27:44 +0900

    타임리프 - 기본기능 :  리터럴

--- src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -70,6 +70,11 @@ public class BasicController {
     }
 
 
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
 
 
 
